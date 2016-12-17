8777b35706ad4e8b21528af1d4063d036ea68f7e
Teus Benschop <teusjannette@gmail.com>
2016-12-17 12:14:52 +0100

    Saves and loads files through Pepper file system

--- native/bibledit.cpp
+++ native/bibledit.cpp
@@ -63,23 +63,132 @@ using namespace std;
 
 pp::Instance * bibledit_instance;
 thread * bibledit_worker_thread;
+// Pointer to the file system. Is null on failure.
 pp::FileSystem * pepper_file_system;
+
+
+void pepper_file_save (const string& file_name, const string& file_contents)
+{
+
+  if (!pepper_file_system) {
+    cerr << "File system is not open " << PP_ERROR_FAILED << endl;
+    return;
+  }
+  
+  pp::FileRef ref (* pepper_file_system, file_name.c_str());
+  pp::FileIO file (bibledit_instance);
+
+  int32_t open_result = file.Open (ref, PP_FILEOPENFLAG_WRITE | PP_FILEOPENFLAG_CREATE | PP_FILEOPENFLAG_TRUNCATE, pp::BlockUntilComplete());
+  if (open_result != PP_OK) {
+    cerr << "File open for write failed " << open_result << endl;
+    return;
+  }
+
+  // We have truncated the file to 0 bytes. So we need only write if file_contents is non-empty.
+  if (!file_contents.empty()) {
+    if (file_contents.length() > INT32_MAX) {
+      cerr << "File too big " << PP_ERROR_FILETOOBIG << endl;
+      return;
+    }
+    int64_t offset = 0;
+    int32_t bytes_written = 0;
+    do {
+      bytes_written = file.Write (offset, file_contents.data() + offset, file_contents.length(), pp::BlockUntilComplete());
+      if (bytes_written > 0) {
+        offset += bytes_written;
+      } else {
+        cerr << "File write failed " << bytes_written << endl;
+        return;
+      }
+    } while (bytes_written < static_cast<int64_t>(file_contents.length()));
+  }
+  
+  // All bytes have been written, flush the write buffer to complete
+  int32_t flush_result = file.Flush (pp::BlockUntilComplete());
+  if (flush_result != PP_OK) {
+    cerr << "File fail to flush " << flush_result << endl;
+    return;
+  }
+  cout << "Save success" << endl;
+}
+
+
+void pepper_file_load (const string& file_name) {
+
+  if (!pepper_file_system) {
+    cerr << "File system is not open "  << PP_ERROR_FAILED << endl;
+    return;
+  }
+
+  pp::FileRef ref (* pepper_file_system, file_name.c_str());
+  pp::FileIO file (bibledit_instance);
+  
+  int32_t open_result = file.Open (ref, PP_FILEOPENFLAG_READ, pp::BlockUntilComplete());
+  if (open_result == PP_ERROR_FILENOTFOUND) {
+    cerr <<  "File not found " << open_result << endl;
+    return;
+  } else if (open_result != PP_OK) {
+    cerr <<  "File open for read failed " << open_result << endl;
+    return;
+  }
+  PP_FileInfo info;
+  int32_t query_result = file.Query (&info, pp::BlockUntilComplete ());
+  if (query_result != PP_OK) {
+    cerr << "File query failed " << query_result << endl;
+    return;
+  }
+  // FileIO.Read() can only handle int32 sizes
+  if (info.size > INT32_MAX) {
+    cerr << "File too big " << PP_ERROR_FILETOOBIG << endl;
+    return;
+  }
+  
+  std::vector<char> data (info.size);
+  int64_t offset = 0;
+  int32_t bytes_read = 0;
+  int32_t bytes_to_read = info.size;
+  while (bytes_to_read > 0) {
+    bytes_read = file.Read (offset, &data[offset], data.size() - offset, pp::BlockUntilComplete());
+    if (bytes_read > 0) {
+      offset += bytes_read;
+      bytes_to_read -= bytes_read;
+    } else if (bytes_read < 0) {
+      // If bytes_read < PP_OK then it indicates the error code.
+      cerr << "File read failed " << bytes_read << endl;
+      return;
+    }
+  }
+  // Done reading, send content to the user interface.
+  string string_data (data.begin(), data.end());
+  cout << string_data << endl;
+  cout << "Load success" << endl;
+}
 
 
 void bibledit_worker_thread_function ()
 {
   cout << "Thread start" << endl;
 
+  // Open the file system on this thread.
+  // Since this is the first operation we perform there,
+  // and because we do everything on this thread synchronously,
+  // this ensures that the FileSystem is open before any FileIO operations execute.
   // Request 10 Gbyte of space in the persistent local filesystem.
   // The Chrome app's manifest requests the "unlimitedStorage" permission already.
   // Google Chrome stores its persistent files in this folder:
   // ~/Library/Application\ Support/Google/Chrome/Default/File\ System
+  // File names for operations on this file system should start with a slash ("/").
+  // Run $ du -chs to find total disk usage.
   pepper_file_system = new pp::FileSystem (bibledit_instance, PP_FILESYSTEMTYPE_LOCALPERSISTENT);
   int result = pepper_file_system->Open (10 * 1024 * 1024 * 1024, pp::BlockUntilComplete());
   if (result != PP_OK) {
     pepper_file_system = nullptr;
-    cout << "Failed to open local persistent file system with error " << result << endl;
+    cerr << "Failed to open local persistent file system with error " << result << endl;
   }
+  pepper_file_save ("/filename.txt", "Contents for the text file");
+  pepper_file_load ("/filename.txt");
+  
+  
   cout << "Thread complete" << endl;
 }
 
